c5bb205df80eef17c7a26d155a64c7fed1b08a48
Spedon Wen <realsped0n@outlook.com>
2024-09-17 16:50:19 +0800

    feat: implement quantized ECG classification model
    - Introduce a new quantized neural network model for ECG classification
    - Define several quantized layers and modules including `QuantEmbedding`, `QuantMLP`, `QuantClassifier`, and `QuantEncoderLayer`
    - Implement the `QuantECGformer` model which integrates these quantized layers
    - Create a training script `qtrain.py` to train the `QuantECGformer` model
    - Define a `NPYDataset` class to load and preprocess data from `.npy` files
    - Implement a `Trainer` class to handle training, validation, and checkpoint saving
    - Configure training parameters and save checkpoints at specified intervals
    
    Signed-off-by: Spedon Wen <realsped0n@outlook.com>

---
+++ qmodel.py
@@ -0,0 +1,117 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+import brevitas.nn as qnn
+
+
+class QuantEmbedding(nn.Module):
+    def __init__(self, signal_channels: int, embed_size: int):
+        super().__init__()
+        self.linear = nn.Linear(signal_channels, embed_size)
+        self.lieanr1 = qnn.QuantLinear(
+            signal_channels,
+            embed_size,
+        )
+        self.norm = nn.LayerNorm(embed_size)
+        self.activation = qnn.QuantReLU()
+
+    def forward(self, x: Tensor) -> Tensor:  # pyright: ignore[reportImplicitOverride]
+        x = self.linear(x)
+        x = self.norm(x)
+        x = self.activation(x)
+        return x
+
+
+class QuantMLP(nn.Module):
+    def __init__(self, embed_size: int, expansion: int):
+        super().__init__()
+        self.linear1 = qnn.QuantLinear(embed_size, embed_size * expansion)
+        self.activation = qnn.QuantReLU()
+        self.linear2 = qnn.QuantLinear(embed_size * expansion, embed_size)
+
+    def forward(self, x: Tensor) -> Tensor:  # pyright: ignore[reportImplicitOverride]
+        x = self.linear1(x)
+        x = self.activation(x)
+        x = self.linear2(x)
+        return x
+
+
+class QuantClassifier(nn.Module):
+    def __init__(self, embed_size: int, classes: int):
+        super().__init__()  # pyright: ignore[reportUnknownMemberType]
+        self.linear1 = qnn.QuantLinear(embed_size, classes)
+        self.norm = nn.LayerNorm(embed_size)
+
+    def forward(self, x: Tensor) -> Tensor:  # pyright: ignore[reportImplicitOverride]
+        x = torch.mean(x, dim=1)
+        x = self.norm(x)
+        x = self.linear1(x)
+        return x
+
+
+class QuantEncoderLayer(nn.Module):
+    def __init__(self, embed_size: int, heads: int, expansion: int, dropout: float):
+        super().__init__()  # pyright: ignore[reportUnknownMemberType]
+        self.embed_size = embed_size
+        self.attention = qnn.QuantMultiheadAttention(
+            embed_size, heads, batch_first=True
+        )
+        self.norm1 = nn.LayerNorm(embed_size)
+        self.dropout1 = nn.Dropout(dropout)
+        self.mlp = QuantMLP(embed_size, expansion)
+        self.norm2 = nn.LayerNorm(embed_size)
+        self.dropout2 = nn.Dropout(dropout)
+
+    def forward(self, x: Tensor) -> Tensor:  # pyright: ignore[reportImplicitOverride]
+        residual = x
+        x = self.norm1(x)
+        x = self.attention(x, x, x)[0]
+        x = self.dropout1(x)
+        x += residual
+
+        residual = x
+        x = self.norm2(x)
+        x = self.mlp(x)
+        x = self.dropout2(x)
+        x += residual
+
+        return x
+
+
+class QuantECGformer(nn.Module):
+    def __init__(
+        self,
+        signal_length: int,
+        signal_channels: int,
+        classes: int,
+        embed_size: int,
+        encoder_layers_num: int,
+        encoder_heads: int,
+        mlp_expansion: int,
+        dropout: float,
+    ):
+        super().__init__()  # pyright: ignore[reportUnknownMemberType]
+        self.embedding = QuantEmbedding(signal_channels, embed_size)
+        self.encoder_layers = nn.ModuleList(
+            [
+                QuantEncoderLayer(embed_size, encoder_heads, mlp_expansion, dropout)
+                for _ in range(encoder_layers_num)
+            ]
+        )
+        self.classifier = QuantClassifier(embed_size, classes)
+        self.positional_embedding = qnn.QuantEmbedding(signal_length, embed_size)
+
+    def forward(self, x: Tensor) -> Tensor:  # pyright: ignore[reportImplicitOverride]
+        x = self.embedding(x)
+
+        positions = (
+            torch.arange(x.size(1), device=x.device).unsqueeze(0).expand(x.size(0), -1)
+        )
+        pos_embed = self.positional_embedding(positions)
+        x = x + pos_embed
+
+        for encoder_layer in self.encoder_layers:
+            x = encoder_layer(x)
+        x = self.classifier(x)
+        return x
---
+++ qtrain.py
@@ -0,0 +1,148 @@
+import json
+from dataclasses import asdict
+from datetime import datetime
+from pathlib import Path
+from typing import Any
+
+import numpy as np
+import torch
+import torch.nn as nn
+from numpy.typing import NDArray
+from torch import Tensor
+from torch.utils.data import DataLoader, Dataset, random_split
+from tqdm import tqdm
+
+from config import Config
+from qmodel import QuantECGformer
+
+
+class NPYDataset(Dataset[Any]):
+    def __init__(self, targets: list[str]) -> None:
+        super().__init__()
+        data_cache: NDArray[np.float32] | None = None
+        labels_cache: list[int] = []
+        for index, target in enumerate(targets):
+            data: NDArray[np.float32] = np.load(f"./assets/{target}.npy")
+            if data_cache is None:
+                data_cache = data
+            else:
+                data_cache = np.vstack((data_cache, data))
+            labels_cache += [index for _ in range(data.shape[0])]
+        self.__data: Tensor = torch.from_numpy(data_cache).float()
+        self.__labels: Tensor = torch.tensor(labels_cache).long()
+
+    def __len__(self) -> int:
+        return len(self.__labels)
+
+    def __getitem__(self, index: int) -> tuple[Tensor, Tensor]:
+        return self.__data[index].unsqueeze(0), self.__labels[index]
+
+
+class Trainer:
+    def __init__(self, config: Config, dataset: NPYDataset) -> None:
+        # data preparation
+        train_size = int(config.train_proportion * len(dataset))
+        val_size = len(dataset) - train_size
+        train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+        self.__batch_size = config.dl_batch_size
+        self.__train_loader = DataLoader(
+            train_dataset,
+            batch_size=self.__batch_size,
+            shuffle=True,
+            num_workers=config.dl_num_workers,
+            pin_memory=True,
+        )
+        self.__val_loader = DataLoader(
+            val_dataset,
+            batch_size=self.__batch_size,
+            shuffle=False,
+            num_workers=config.dl_num_workers,
+            pin_memory=True,
+        )
+        # model
+        self.__model = QuantECGformer(
+            config.model.signal_length,
+            config.model.signal_channels,
+            config.model.classes,
+            config.model.embed_size,
+            config.model.encoder_layers_num,
+            config.model.encoder_heads,
+            config.model.mlp_expansion,
+            config.model.dropout,
+        )
+        # optimizer
+        self.__optimizer = torch.optim.AdamW(  # pyright: ignore[reportPrivateImportUsage]
+            self.__model.parameters(), lr=config.lr, weight_decay=config.weight_decay
+        )
+        # loss function
+        self.__criterion = nn.CrossEntropyLoss()
+        # device
+        self.__device = torch.device(config.device)
+        self.__model.to(self.__device)
+        # others
+        self.__epochs = config.epochs
+        self.__validation_interval = config.validation_interval
+        self.__save_interval = config.save_interval
+        self.__start_timestamp = datetime.now().strftime("%Y%m%d%H%M")
+        self.__save_dir = Path(f"./qcheckpoints/{self.__start_timestamp}/")
+        # create save directory if not exists
+        if not self.__save_dir.exists():
+            self.__save_dir.mkdir(parents=True, exist_ok=True)
+        # print config
+        config_json = json.dumps(asdict(config), indent=4)
+        print(config_json)
+        print()
+        # save config info to checkpoint directory
+        with open(self.__save_dir.joinpath("config.json"), "w") as f:
+            f.write(config_json)
+
+    def __train_epoch(self, epoch: int) -> None:
+        self.__model.train()
+        loader = tqdm(self.__train_loader)
+        accuracy = 0
+        for data in loader:
+            self.__optimizer.zero_grad()
+            signal, label = [d.to(self.__device) for d in data]
+            prediction = self.__model(signal.permute(0, 2, 1))
+            loss = self.__criterion(prediction, label)
+            loss.backward()
+            self.__optimizer.step()
+            accuracy += torch.sum(prediction.argmax(1) == label)
+            loader.set_description(
+                f"TRAINING: {epoch}, loss: {loss.item()}. Target: {label[:8].tolist()}, Prediction: {prediction.argmax(1)[:8].tolist()}"
+            )
+        print(f"TRAINING Accuracy: {accuracy / len(loader) / self.__batch_size}")
+        print()
+
+    @torch.no_grad()  # pyright: ignore[reportUntypedFunctionDecorator]
+    def __validation_epoch(self, epoch: int) -> None:
+        self.__model.eval()
+        loader = tqdm(self.__val_loader)
+        accuracy = 0
+        for data in loader:
+            signal, label = [d.to(self.__device) for d in data]
+            prediction = self.__model(signal.permute(0, 2, 1))
+            accuracy += torch.sum(prediction.argmax(1) == label)
+            loader.set_description(
+                f"VALIDATION: {epoch}, Target: {label[:8].tolist()}, Prediction: {prediction.argmax(1)[:8].tolist()}"
+            )
+        print(f"VALIDATION Accuracy: {accuracy / len(loader) / self.__batch_size}")
+        print()
+
+    def train(self) -> None:
+        for epoch in range(1, self.__epochs + 1):
+            self.__train_epoch(epoch)
+            if epoch % self.__validation_interval == 0:
+                self.__validation_epoch(epoch)
+            if epoch % self.__save_interval == 0:
+                torch.save(
+                    self.__model.state_dict(),
+                    f"{self.__save_dir}/model_{epoch}.pth",
+                )
+
+
+if __name__ == "__main__":
+    config = Config()
+    dataset = NPYDataset(["N", "A", "V", "L", "R"])
+    trainer = Trainer(config, dataset)
+    trainer.train()
